c8b2e4036a4ead883c1f5075b43d955fcc68c411
tf <info@tf-k.org>
2014-07-12 19:57:40 +0400

    правки адресов

--- jdjiwi.org.core/_.system/pages/include.php
+++ jdjiwi.org.core/_.system/pages/include.php
@@ -3,9 +3,13 @@
 /* конфигурация */
 cConfig::load('pages');
 cConfig::load('base');
+
+/* заагрузка библиотек */
 cLoader::library('pages:cPages');
+self::config('const');
+
+/* конфигурация */
 cPages::base()->set(cConfig::get('base.host'));
 self::config('sql.table');
 cConfig::load('url');
-self::config('const');
 ?>
