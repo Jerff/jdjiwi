2befc8e37930b2a21f97d42ec6f83165b05f34fb
tf <info@tf-k.org>
2014-07-17 04:52:04 +0400

    правки роутера

--- jdjiwi.org.core/_.system/pages/config/const.php
+++ jdjiwi.org.core/_.system/pages/config/const.php
@@ -4,13 +4,9 @@
  * константы быстрого доступа
  */
 // устанавливаем адреса разделов
-define('cAppUrl', cPages::base()->application);
-define('cAdminUrl', cPages::base()->admin);
-define('cCompileUrl', cPages::base()->compileJsCss);
-define('cItemUrl', cPages::base()->router());
-
-// устанавливаем адреса для быстрого доступа к каталогам проекта
-define('cBaseAppUrl', cAppUrl . '/');
-define('cBaseAdminUrl', cAdminUrl . '/');
-define('cBaseImgUrl', cBaseAppUrl);
+define('cAppUrl', cPages::base()->application . '/');
+define('cAdminUrl', cPages::base()->admin . '/');
+define('cCompileUrl', cPages::base()->compileJsCss . '/');
+define('cItemUrl', cPages::base()->router() . '/');
+define('cImgUrl', cBaseAppUrl);
 ?>
