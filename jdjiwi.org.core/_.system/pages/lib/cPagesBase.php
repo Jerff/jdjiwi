05d0e8ebbb44c82a590a34d80a5c459072cf141c
tf <info@tf-k.org>
2014-08-03 16:16:46 +0400

    правки роутера

--- jdjiwi.org.core/_.system/pages/lib/cPagesBase.php
+++ jdjiwi.org.core/_.system/pages/lib/cPagesBase.php
@@ -10,26 +10,33 @@ class cPagesBase {
 
     public function router() {
         $this->mBase = cCOnfig::get('router.list');
-        if (defined('cApplication')) {
-            if (isset($this->mBase[cApplication])) {
-                return $this->mBase[cApplication];
-            } else {
-                throw new cException('не найден раздел сайта', cApplication);
-                exit;
+        try {
+            if (defined('cApplication')) {
+                if (isset($this->mBase[cApplication])) {
+                    return $this->mBase[cApplication];
+                } else {
+                    throw new cException('не найден раздел сайта', cApplication);
+                    exit;
+                }
+            }
+            $mSearch = array();
+            $url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            foreach ($this->mBase as $key => $value) {
+                if (strpos($url, $value) !== false) {
+                    $mSearch[cString::strlen($value)] = $key;
+                }
+            }
+            if (empty($mSearch)) {
+                throw new cException('неправильный раздел сайта');
+            }
+            krsort($mSearch);
+            define('cApplication', each($mSearch)['value']);
+        } catch (cException $e) {
+            cLog::errorLog($e);
+            if (!defined('cApplication')) {
+                define('cApplication', 'application');
             }
         }
-        $mSearch = array();
-        $url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        foreach ($this->mBase as $key => $value) {
-            if (strpos($url, $value) !== false) {
-                $mSearch[cString::strlen($value)] = $key;
-            }
-        }
-        if(empty($mSearch)) {
-            throw new cException('неправильный раздел сайта');            
-        }
-        krsort($mSearch);
-        define('cApplication', each($mSearch)['value']);
         return $this->mBase[cApplication];
     }
 
